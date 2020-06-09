505464dfa2bb5f55786be4c03b525df052e93ec5
lawiraharlan <lawiraharlan@gmail.com>
2020-06-09 13:45:19 +0700

    Finish!

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -12,7 +12,7 @@
                         <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Project Management System</h1>
+                                    <h1 class="h4 text-gray-900 mb-4"><strong>Project Management System</strong></h1>
                                 </div>
                                 <!-- Flash data -->
                                 <?php $this->view('message') ?>
